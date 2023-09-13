472e52065081daf6b1f82461f114fe2bdc6583c7
Dominik Brezovšek <dominik.brezovsek.17339@dijak.sc-celje.si>
2023-09-13 11:14:50 +0200

    Added some missing imports😅

--- API/app/Http/Controllers/TeacherController.php
+++ API/app/Http/Controllers/TeacherController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TeacherModel;
 use Illuminate\Http\Request;
 
 class TeacherController extends Controller
@@ -23,6 +24,6 @@ class TeacherController extends Controller
         return response()->json([
             "message" => "teacher record created"
         ], 201);
-        
+
     }
 }
--- API/routes/web.php
+++ API/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 
