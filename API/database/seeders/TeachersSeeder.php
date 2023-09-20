7698b86d77c9283c8982c2ee5d48885dd7fa22a5
Dominik Brezovšek <dominik.brezovsek.17339@dijak.sc-celje.si>
2023-09-21 00:37:22 +0200

    Added seeders for testing purposes

--- API/database/seeders/TeachersSeeder.php
+++ API/database/seeders/TeachersSeeder.php
@@ -13,8 +13,15 @@ class TeachersSeeder extends Seeder
      */
     public function run(): void
     {
+        $teacher = new TeacherModel();
+        $teacher->ime = "Admin";
+        $teacher->priimek = "KER";
+        $teacher->kabinet = "BE08";
+        $teacher->email = "admin@ker.sc-celje.si";
+        $teacher->save();
+
         $faker = \Faker\Factory::create();
-        for ($i = 0; $i < 100; $i++) {
+        for ($i = 0; $i < 10; $i++) {
             TeacherModel::create([
                 'ime' => $faker->firstName,
                 'priimek' => $faker->lastName,
