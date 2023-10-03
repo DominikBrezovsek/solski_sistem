eeb58a166d309164f934a7bfd3e6d13c3c03c002
Dominik Brezovšek <dominik.brezovsek.17339@dijak.sc-celje.si>
2023-10-03 11:17:47 +0200

    Working on protection of the routes

--- API/routes/api.php
+++ API/routes/api.php
@@ -32,10 +32,10 @@ Route::prefix('student/')->group(function (){
 })->middleware('verifyToken');
 
 Route::prefix('school/')->group(function (){
-    Route::get('getAll', [SchoolControler::class, 'getSchool']);
+    Route::post('getAll', [SchoolControler::class, 'getSchool']);
     Route::post('classes', [SchoolControler::class, 'getClass']);
 })->middleware('verifyToken');
 
 Route::prefix('teacher/')->group(function (){
-    Route::get('get', [TeacherController::class, 'getTeacher']);
+    Route::post('get', [TeacherController::class, 'getTeacher']);
 })->middleware('verifyToken');
