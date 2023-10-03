c4d82c560315b7065c68cef19c7c4402c00f7dea
Dominik Brezovšek <dominik.brezovsek.17339@dijak.sc-celje.si>
2023-10-03 11:43:50 +0200

    Added token with requests.

--- BSS/src/components/Sidebar.vue
+++ BSS/src/components/Sidebar.vue
@@ -1,121 +1,132 @@
 <template>
-    <div class="navbar">
-        <div class="logo">
-          <div>
-            <img src="https://smv.usdd.company/images/logo.png" alt="logo">
-          </div>
-        </div>
-        <div class="user">
-            <img :src="image" alt="user">
-            <div class="user-info">
-                <p>{{name}} {{surname}}</p>
-                <p v-if="type == 'student'">Razred: {{class}}</p>
-                <p>Šolsko leto {{school_year}}</p>
-            </div>
-        </div>
-        <div class="menu">
-          <li>A</li>
-          <li>A</li>
-          <li>A</li>
-        </div>
+  <div class="navbar">
+    <div class="logo">
+      <div>
+        <img src="https://smv.usdd.company/images/logo.png" alt="logo">
+      </div>
     </div>
+    <div class="user">
+      <img :src="image" alt="user">
+      <div class="user-info">
+        <p>{{ name }} {{ surname }}</p>
+        <p v-if="type == 'student'">Razred: {{ class }}</p>
+        <p>Šolsko leto {{ school_year }}</p>
+      </div>
+    </div>
+    <div class="menu">
+      <li>A</li>
+      <li>A</li>
+      <li>A</li>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import axios from 'axios';
+import router from "@/router";
 
 
-
-export default  {
-    data() {
-        return {
-            name: "",
-            surname: "",
-            class: "",
-            image: "",
-            school_year: "",
-            type: "",
-        }
-    },
-    methods: {
-        getSchoolYear() {
-            let date = new Date();
-            let year = date.getFullYear();
-            let month = date.getMonth();
-            let nextYear = new Date(new Date().setFullYear(new Date().getFullYear() + 1)).getFullYear()
-
-          if (month < 8) {
-                this.school_year = (year - 1 + "/" + year);
-            }
-            else {
-                this.school_year = (year + "/" + nextYear);
-            }
-        },
-        getUserImage(tip:string){
-            if(tip == "student"){
-                return "https://smv.usdd.company/images/studenti.png";
-            }
-            else if(tip == "teacher"){
-                return "https://smv.usdd.company/images/ucitelji.png";
-            }
-            else{
-                return "https://smv.usdd.company/images/default.png";
-            }
-        },
-
-        getUser(tip:string){
-            const user = new FormData();;
-            switch (tip){
-                case "student":{
-                  this.type = tip;
-                    axios.get('https://smv.usdd.company/API/public/api/student/get')
-                    .then((response) => {
-                        this.name = response.data.name;
-                        this.surname = response.data.surname;
-                        this.class = response.data.class;
-                    }, (error) => {
-                        console.log(error);
-                    });
-                    break
-                }
-                case "teacher":{
-                  this.type = tip;
-                    axios.get('https://smv.usdd.company/API/public/api/teacher/get',)
-                    .then((response) => {
-                        this.name = response.data.name;
-                        this.surname = response.data.surname;
-                    }, (error) => {
-                        console.log(error);
-                    });
-                    break
-                }
-                case "admin": {
-                  this.type = tip;
-                    axios.get('https://smv.usdd.company/API/public/api/admin/get')
-                    .then((response) => {
-                        this.name = response.data.name;
-                        this.surname = response.data.surname;
-                    }, (error) => {
-                        console.log(error);
-                    });
-                    break
-                }
-
-
-            }
-        }
-        
-    },
-    created() {
-        this.getSchoolYear();
-        const type = sessionStorage.getItem('type');
-        if ( type != null ) {
-            this.image = this.getUserImage(type);
-            this.getUser(type);
-        }
-
+export default {
+  data() {
+    return {
+      name: "",
+      surname: "",
+      class: "",
+      image: "",
+      school_year: "",
+      type: "",
     }
-    
+  },
+  methods: {
+    getSchoolYear() {
+      let date = new Date();
+      let year = date.getFullYear();
+      let month = date.getMonth();
+      let nextYear = new Date(new Date().setFullYear(new Date().getFullYear() + 1)).getFullYear()
+
+      if (month < 8) {
+        this.school_year = (year - 1 + "/" + year);
+      } else {
+        this.school_year = (year + "/" + nextYear);
+      }
+    },
+    getUserImage(tip: string) {
+      if (tip == "student") {
+        return "https://smv.usdd.company/images/studenti.png";
+      } else if (tip == "teacher") {
+        return "https://smv.usdd.company/images/ucitelji.png";
+      } else {
+        return "https://smv.usdd.company/images/default.png";
+      }
+    },
+
+    getUser(tip: string) {
+      const token = new FormData();
+      const localToken = sessionStorage.getItem('token')
+      if (localToken != null) {
+        token.append('token', localToken)
+      }
+      switch (tip) {
+        case "student": {
+          this.type = tip;
+          axios.post('https://smv.usdd.company/API/public/api/student/get', token)
+              .then((response) => {
+                if (response.data.name != "") {
+                  this.name = response.data.name;
+                  this.surname = response.data.surname;
+                  this.class = response.data.class;
+                } else if (response.data.error == "token") {
+                  sessionStorage.clear();
+                  this.$router.push('/');
+                }
+              }, (error) => {
+                console.log(error);
+              });
+          break
+        }
+        case "teacher": {
+          this.type = tip;
+          axios.post('https://smv.usdd.company/API/public/api/teacher/get', token)
+              .then((response) => {
+                if (response.data.name != "") {
+                  this.name = response.data.name;
+                  this.surname = response.data.surname;
+                } else if (response.data.error == "token") {
+                  sessionStorage.clear();
+                  this.$router.push('/');
+                }
+              }, (error) => {
+                console.log(error);
+              });
+          break
+        }
+        case "admin": {
+          this.type = tip;
+          axios.post('https://smv.usdd.company/API/public/api/admin/get', token)
+              .then((response) => {
+                this.name = response.data.name;
+                this.surname = response.data.surname;
+              }, (error) => {
+                console.log(error);
+              });
+          break
+        }
+
+
+      }
+    }
+
+  },
+  created() {
+    this.getSchoolYear();
+    const type = sessionStorage.getItem('type');
+    if (type != null) {
+      this.image = this.getUserImage(type);
+      this.getUser(type);
+    }
+
+  }
+
 }
 </script>
 
@@ -131,7 +142,7 @@ export default  {
   justify-content: space-evenly;
 }
 
-.logo{
+.logo {
   margin-bottom: 20vh;
   width: 100%;
   display: flex;
@@ -139,15 +150,15 @@ export default  {
   justify-content: center;
 }
 
-.logo img{
+.logo img {
   height: 15vh;
 }
 
-.user img{
+.user img {
   height: 10vh;
 }
 
-.user{
+.user {
   margin-bottom: 25vh;
   width: 100%;
   display: flex;
